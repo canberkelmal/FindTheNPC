ed7791f888468ce479ac5160372f289871e78fae
canberkelmal <elmalcanberk@gmail.com>
2022-10-28 17:10:02 +0300

    animated

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,12 +7,16 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     NavMeshAgent enMesh;
+    Animator animator;
     public GameObject targetObject1;
     public GameObject targetObject2;
     public GameObject marker;
     public Vector3 dest;
     public Text reachCounter;
     public bool mrk=true;
+    bool isWalking;
+    float v;
+    int isWalkingHash;
     void Start()
     {
         enMesh = GetComponent<NavMeshAgent>();
@@ -20,11 +24,23 @@ public class EnemyMovement : MonoBehaviour
         RandomPos();
         enMesh.destination=dest;
         reachCounter.text="0";
+        animator=transform.GetChild(0).GetComponent<Animator>();
+        isWalkingHash=Animator.StringToHash("isWalking");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        isWalking=animator.GetBool(isWalkingHash);
+        v=enMesh.velocity.sqrMagnitude;
+        if(v != 0f && !isWalking){
+            animator.SetBool(isWalkingHash, true);
+        }
+        if(v == 0f && isWalking){
+            animator.SetBool(isWalkingHash, false);
+        }
+
         if(enMesh.velocity.sqrMagnitude == 0f && mrk){
             RandomPos();
             enMesh.destination=dest;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,21 +9,51 @@ public class PlayerMovement : MonoBehaviour
     
     
     NavMeshAgent pMesh;
+    public GameObject enemy;
     public Vector3 target;
     public Text hitCounter;
     public LayerMask layerMask;
+    Animator animator;
+    float v;
+    int isWalkingHash;
+    int isCatchHash;
+    bool isWalking;
+    bool isCatch;
+    bool isAttacked=false;
 
     void Start()
     {
         pMesh = GetComponent<NavMeshAgent>();
         hitCounter.text="0";
+        animator=transform.GetChild(0).GetComponent<Animator>();
+        isWalkingHash=Animator.StringToHash("isWalking");
+        isCatchHash=Animator.StringToHash("isCatch");
         //target=new Vector3(6, 0.5f, 6);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.DrawRay(transform.position, Vector3.forward, Color.red);
+        //Debug.DrawRay(transform.position, Vector3.forward, Color.red);
+
+        //Debug.Log(pMesh.velocity.sqrMagnitude);
+        isWalking=animator.GetBool(isWalkingHash);
+        isCatch=animator.GetBool(isCatchHash);
+        v=pMesh.velocity.sqrMagnitude;
+        if(v != 0f && !isWalking){
+            animator.SetBool(isWalkingHash, true);
+        }
+        if(v == 0f && isWalking){
+            animator.SetBool(isWalkingHash, false);
+        }
+        if(isCatch){
+            animator.SetBool(isCatchHash, false);
+            isAttacked=true;
+        }
+        if(isAttacked && isWalking){
+            isAttacked=false;
+            teleportEnemy();
+        }
 
         if(Input.GetMouseButton(0)){
 
@@ -32,11 +62,11 @@ public class PlayerMovement : MonoBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
                 pMesh.destination = hit.point;
                 target=hit.point;
-                Debug.Log(hit.transform.name);
-                Debug.DrawRay(transform.position, Vector3.forward, Color.red);
+                //Debug.Log(hit.transform.name);
+                //Debug.DrawRay(transform.position, Vector3.forward, Color.red);
             }
             if(Physics.Raycast(transform.position, Vector3.forward, out hit, 5f, layerMask)){
-                Debug.Log(hit.transform.name);
+                //Debug.Log(hit.transform.name);
             }
 
             //target=Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -47,9 +77,13 @@ public class PlayerMovement : MonoBehaviour
 
     void OnTriggerEnter(Collider other){
         if(other.gameObject.tag=="Enemy"){
-            Debug.Log("hit");
-            other.gameObject.transform.position=new Vector3(Random.Range(-7,7), 0.5f, Random.Range(-7,7));
-            hitCounter.text=(int.Parse(hitCounter.text)+1).ToString();
+            
+            animator.SetBool(isCatchHash, true);
+            Debug.Log("attack!");
         }
     }
+     void teleportEnemy(){
+        enemy.gameObject.transform.position=new Vector3(Random.Range(-7,7), 0.5f, Random.Range(-7,7));
+        hitCounter.text=(int.Parse(hitCounter.text)+1).ToString();        
+     }
 }
